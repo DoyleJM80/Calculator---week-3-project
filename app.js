aa4a621f1ec3e5d0f596e771f4e178871ac4a36e
Jamie Doyle <thedoyles922@users.noreply.github.com>
2017-06-09 13:31:12 -0400

    created 4 functions

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 (function(){
   'use strict';
 
+  var addition = function(num1, num2) {
+    return num1 + num2;
+  };
 
+  var subtraction = function (num1, num2) {
+    return num1 - num2;
+  };
+
+  var multiplication = function (num1, num2) {
+    return num1 * num2;
+  };
+
+  var division = function (num1, num2) {
+    return num1 / num2;
+  };
 
 
 
